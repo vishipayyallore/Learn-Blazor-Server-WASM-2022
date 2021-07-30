a95d5f0b08aecc9c93ff72de080316d6f39bfc13
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-31 00:31:40 +0530

    Today's work

---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Pages/1HelloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASM.BasicsDemo.Pages._1HelloWorld
+{
+
+    public partial class HelloWorld
+    {
+        [Parameter]
+        public string UserName { get; set; }
+    }
+
+}
