f3d3ecf6eea057fcdb425378d33a7dc39dcec69a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-02 07:52:30 +0530

    Today's work

---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Pages/2RouteParameter/RoutingDemo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASM.BasicsDemo.Pages._2RouteParameter
+{
+    public partial class RoutingDemo
+    {
+
+        [Parameter]
+        public string Username { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Username = Username ?? "No Name";
+        }
+
+    }
+
+}
