2534a5a53999f83ec0b0c93ef9b04b74cc416c70
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-03 00:12:07 +0530

    Today's work

---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Pages/3CatchAll/CatchAll.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASM.BasicsDemo.Pages._3CatchAll
+{
+
+    public partial class CatchAll
+    {
+        [Parameter]
+        public string PageRoute { get; set; }
+    }
+
+}
