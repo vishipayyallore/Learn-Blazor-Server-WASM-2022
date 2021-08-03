f3938e25a9fbea471b4f7ca884468413686c1b9d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-03 23:39:36 +0530

    Today's work

---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Model/HotelRoom.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorWASM.BasicsDemo.Model
+{
+
+    public class HotelRoom
+    {
+        public int Id { get; set; }
+
+        public string RoomName { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsActive { get; set; }
+
+        // public List<BlazorRoomProp> RoomProps { get; set; }
+    }
+
+}
