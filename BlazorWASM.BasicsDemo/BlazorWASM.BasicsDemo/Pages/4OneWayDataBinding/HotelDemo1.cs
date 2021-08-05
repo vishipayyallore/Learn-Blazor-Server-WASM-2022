940027e2c07f0bfd36d3300cd67c1527ae2a1a6d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-05 22:52:08 +0530

    Today's work

---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Pages/4OneWayDataBinding/HotelDemo1.cs
@@ -0,0 +1,22 @@
+﻿using BlazorWASM.BasicsDemo.Model;
+
+namespace BlazorWASM.BasicsDemo.Pages._4OneWayDataBinding
+{
+
+    public partial class HotelDemo1
+    {
+        public HotelRoom DemoRoom { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            DemoRoom = new HotelRoom
+            {
+                Id = 1,
+                RoomName = "Villa Suite",
+                IsActive = true,
+                Price = 499
+            };
+        }
+    }
+
+}
---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Pages/5TwoWayDataBinding/HotelDemo2.cs
@@ -0,0 +1,20 @@
+﻿using BlazorWASM.BasicsDemo.Model;
+
+namespace BlazorWASM.BasicsDemo.Pages._5TwoWayDataBinding
+{
+    public partial class HotelDemo2
+    {
+        public HotelRoom DemoRoom { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            DemoRoom = new HotelRoom
+            {
+                Id = 1,
+                RoomName = "Villa Suite",
+                IsActive = true,
+                Price = 499
+            };
+        }
+    }
+}
