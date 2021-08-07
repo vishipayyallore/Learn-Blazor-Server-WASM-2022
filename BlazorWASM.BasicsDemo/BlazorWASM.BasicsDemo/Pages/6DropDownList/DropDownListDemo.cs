57cf13cfdc898cde78a1209b493ee8d9ec18630f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-07 19:31:55 +0530

    Few Changes

---
+++ BlazorWASM.BasicsDemo/BlazorWASM.BasicsDemo/Pages/6DropDownList/DropDownListDemo.cs
@@ -0,0 +1,25 @@
+﻿using BlazorWASM.BasicsDemo.Model;
+
+namespace BlazorWASM.BasicsDemo.Pages._6DropDownList
+{
+
+    public partial class DropDownListDemo
+    {
+        public HotelRoom DemoRoom { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+
+            DemoRoom = new HotelRoom
+            {
+                Id = 1,
+                RoomName = "Villa Suite",
+                IsActive = true,
+                Price = 499
+            };
+
+        }
+
+    }
+
+}
