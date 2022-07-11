63b8aeff85750c71c977bc7f7d331f104de06b54
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-07-11 23:34:45 +0530

    Initial Version

---
+++ BlazorServerPlaywrightDemo/Tests/HelloBlazorServerApp.E2ETests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HelloBlazorServerApp.E2ETests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
