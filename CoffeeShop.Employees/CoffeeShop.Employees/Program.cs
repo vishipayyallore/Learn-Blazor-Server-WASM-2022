381f9ba8e1a240d44e88722b9d6411a77b9ea836
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-10 00:11:53 +0530

    Employee OverView

--- CoffeeShop.Employees/CoffeeShop.Employees/Program.cs
+++ CoffeeShop.Employees/CoffeeShop.Employees/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddDbContextFactory<EmployeeManagerDbContext>(
-    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeManagerDb")));
+builder.Services.AddDbContextFactory<EmployeeManagerDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeManagerDb")));
 
 var app = builder.Build();
 
