71a665348222cfe822097c52bc7cffe7cbb4f3f0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-09 23:31:22 +0530

    Update Program.cs

--- CoffeeShop.Employees/CoffeeShop.Employees/Program.cs
+++ CoffeeShop.Employees/CoffeeShop.Employees/Program.cs
@@ -13,12 +13,26 @@ builder.Services.AddDbContext<EmployeeManagerDbContext>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    // TODO: To be removed once we have .sqlproj 
+    // WARNING: Don't do this in production, just useful for development
+    using var scope = app.Services.CreateAsyncScope();
+    using var context = scope.ServiceProvider.GetService<EmployeeManagerDbContext>();
+
+    if (!await context!.Database.CanConnectAsync())
+    {
+        _ = await context!.Database.EnsureCreatedAsync();
+    }
+}
+else
 {
     app.UseExceptionHandler("/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
