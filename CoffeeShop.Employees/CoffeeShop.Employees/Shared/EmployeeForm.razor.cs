1a4465a5a5001bfa1d7777ff64c8a5d99f923874
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-20 14:09:27 +0530

    Reusable form

---
+++ CoffeeShop.Employees/CoffeeShop.Employees/Shared/EmployeeForm.razor.cs
@@ -0,0 +1,37 @@
+﻿using CoffeeShop.Data.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace CoffeeShop.Employees.Shared;
+
+public partial class EmployeeForm
+{
+
+    [Parameter]
+    public Employee? Employee { get; set; }
+
+    [Parameter]
+    public Department[]? Departments { get; set; }
+
+    [Parameter]
+    public bool IsBusy { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> OnSubmit { get; set; }
+
+    private async Task HandleValidSubmit()
+    {
+        if (OnSubmit.HasDelegate)
+        {
+            await OnSubmit.InvokeAsync(true);
+        }
+    }
+
+    private async Task HandleInvalidSubmit()
+    {
+        if (OnSubmit.HasDelegate)
+        {
+            await OnSubmit.InvokeAsync(false);
+        }
+    }
+
+}
