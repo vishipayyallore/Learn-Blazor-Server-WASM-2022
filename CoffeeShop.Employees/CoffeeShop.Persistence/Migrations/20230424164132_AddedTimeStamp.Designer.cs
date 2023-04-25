725fe8812e1721aba1d945cf8357dcc493595f23
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-25 09:32:06 +0530

    Swamy/24apr work (#10)
    * Delete Employees
    
    * Update ListEmployees.razor.cs
    
    * Changes
    
    * Package Updates
    
    * Added TimeStamp

--- CoffeeShop.Employees/CoffeeShop.Persistence/Migrations/20230424164132_AddedTimeStamp.Designer.cs
+++ CoffeeShop.Employees/CoffeeShop.Persistence/Migrations/20230424164132_AddedTimeStamp.Designer.cs
@@ -4,6 +4,7 @@ using CoffeeShop.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoffeeShop.Persistence.Migrations
 {
     [DbContext(typeof(EmployeeManagerDbContext))]
-    partial class EmployeeManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230424164132_AddedTimeStamp")]
+    partial class AddedTimeStamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
