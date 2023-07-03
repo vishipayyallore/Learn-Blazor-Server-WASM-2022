d846c175efeee094b177063b7b26e9ae6a8af6aa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-03 08:58:40 +0530

    Leave Type Dto

--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.MappingProfiles;
