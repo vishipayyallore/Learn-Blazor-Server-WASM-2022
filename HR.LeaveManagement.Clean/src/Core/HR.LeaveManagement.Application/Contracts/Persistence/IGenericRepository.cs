eeb1fa385cf807a151f2163380e5b94f1696498e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-03 08:10:11 +0530

    Update IGenericRepository.cs

--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -6,9 +6,9 @@ public interface IGenericRepository<T> where T : class
 
     Task<T> GetByIdAsync(int id);
 
-    Task CreateAsync(T entity);
+    Task<T> CreateAsync(T entity);
 
-    Task UpdateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
 
-    Task DeleteAsync(T entity);
+    Task<T> DeleteAsync(T entity);
 }
