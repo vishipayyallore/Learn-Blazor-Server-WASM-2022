5e4d7edaa9b29dcff30a19bef0c6a3feecb5883c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-05 08:08:42 +0530

    Swamy/04jul cqrs request day1 (#14)
    * Create GetLeaveTypesQuery.cs
    
    * Create GetLeaveTypesQueryHandler.cs

---
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    // private readonly IAppLogger<GetLeaveTypesQueryHandler> _logger;
+
+    public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+        // this._logger = logger;
+    }
+
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        // Query the database
+        var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+        // convert data objects to DTO objects
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+        // return list of DTO object
+        // _logger.LogInformation("Leave types were retrieved successfully");
+
+        return data;
+    }
+}
