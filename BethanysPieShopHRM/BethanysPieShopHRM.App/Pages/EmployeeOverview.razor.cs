6cde58e2bb44860199c7f9b31b2284f1dbddf558
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-14 18:24:12 +0530

    Changes

---
+++ BethanysPieShopHRM/BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.App.Pages;
+
+public partial class EmployeeOverview
+{
+    public List<Employee> Employees { get; set; } = default!;
+
+
+    protected override void OnInitialized()
+    {
+        Employees = MockDataService.Employees;
+    }
+}
