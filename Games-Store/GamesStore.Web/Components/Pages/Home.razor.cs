82a14c8e2103cac5bdf66fb0a9de749c38132732
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-04 17:36:07 +0530

    Create Home.razor.cs

---
+++ Games-Store/GamesStore.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿using GamesStore.Web.Models;
+
+namespace GamesStore.Web.Components.Pages;
+
+public partial class Home
+{
+    private Game[] games =
+    [
+        new Game { Id = 1, Name = "Super Mario Bros", Genre = "Platformer", Price = 59.99M, ReleaseDate = DateTime.Parse("1985-09-13") },
+    ];
+
+}
