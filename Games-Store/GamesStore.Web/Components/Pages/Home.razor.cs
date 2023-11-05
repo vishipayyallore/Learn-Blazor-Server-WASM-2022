f9af2ae2cf1b1c6483e692bd5994790589785645
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-05 09:38:03 +0530

    Swamy/02nov blazor full stackapp day1 (#22)
    * Initial Version
    
    * Update ReadMe.md
    
    * Update index.html
    
    * VS Code Assets
    
    * Update launchSettings.json
    
    * Update MainLayout.razor.css
    
    * Changes
    
    * Changes
    
    * Update app.css
    
    * Changes
    
    * Update Home.razor
    
    * Changes
    
    * Changes
    
    * Create Home.razor.cs
    
    * Changes

---
+++ Games-Store/GamesStore.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿using GamesStore.Web.Models;
+
+namespace GamesStore.Web.Components.Pages;
+
+public partial class Home
+{
+    private Game[] games =
+    [
+        new Game { Id = 1, Name = "Super Mario Bros", Genre = "Platformer", Price = 59.99M, ReleaseDate = new DateTime(1985, 09, 13) },
+        new Game() { Id = 2, Name = "Street Fighter II", Genre = "Fighting", Price = 19.99M, ReleaseDate = new DateTime(1991, 2, 1) },
+        new Game() { Id = 3, Name = "Final Fantasy XIV", Genre = "Roleplaying", Price = 59.99M, ReleaseDate = new DateTime(2010, 9, 30) },
+        new Game() { Id = 4, Name = "FIFA 23", Genre = "Sports", Price = 69.99M, ReleaseDate = new DateTime(2022, 9, 27) },
+        new Game { Id = 5, Name = "Super Mario Bros 2", Genre = "Platformer", Price = 59.99M, ReleaseDate = new DateTime(1988, 10, 09) },
+    ];
+
+}
---
+++ Games-Store/ReadMe.md
@@ -0,0 +1,28 @@
+# Simple Blazor WASM Project
+
+This is a simple Blazor WASM project that I am learning from a Video Course. I am using this project to learn Blazor WASM and to learn how to use GitHub.
+
+## Getting Started
+
+> 1. To be Decided
+
+## GamesStore.Web
+
+> 1. Blazor WASM Front End Project
+
+```powershell
+dotnet new blazorwasm -n GamesStore.Web
+
+dotnet build .\GamesStore.Web\GamesStore.Web.csproj
+dotnet run --project .\GamesStore.Web\GamesStore.Web.csproj
+```
+
+## Few Commands
+
+```powershell
+dotnet --info
+dotnet --list-sdks
+dotnet --version
+dotnet new --help
+dotnet build
+```
