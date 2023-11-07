b4fbfdf7192f42ee54554eeefd3b860d9d8cdc9a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-07 17:20:33 +0530

    Changes

---
+++ Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
@@ -0,0 +1,8 @@
+﻿using GamesStore.Web.Models;
+
+namespace GamesStore.Web.Components.Pages;
+
+public partial class EditGame
+{
+    private Game game = new() { Name = string.Empty, Genre = string.Empty, ReleaseDate = DateTime.UtcNow };
+}
