f3836b1ad9b6f23de74ee0e61c67c67f9775d08f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-10 23:26:17 +0530

    Navigating Back to List Screen

--- Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
+++ Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
@@ -1,15 +1,21 @@
 ﻿using GamesStore.Web.Models;
 using GamesStore.Web.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace GamesStore.Web.Components.Pages;
 
 public partial class EditGame
 {
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
     private Game game = new() { Name = string.Empty, Genre = string.Empty, ReleaseDate = DateTime.UtcNow };
 
     private void HandleSubmit()
     {
         GameClient.AddGame(game);
+
+        NavigationManager.NavigateTo("/");
     }
 
 }
