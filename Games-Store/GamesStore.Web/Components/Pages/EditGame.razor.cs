8f91635f5c0bd31115d7b8425cb830bd4b87fd77
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-10 09:06:40 +0530

    Swamy/09nov blazor full stackapp day2 (#25)
    * Update GameClient.cs
    
    * Update NavMenu.razor
    
    * Changes
    
    * Update NavMenu.razor
    
    * Update NavMenu.razor.css
    
    * Add Game
    
    * Changes
    
    * Changes

--- Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
+++ Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
@@ -1,8 +1,15 @@
 ﻿using GamesStore.Web.Models;
+using GamesStore.Web.Services;
 
 namespace GamesStore.Web.Components.Pages;
 
 public partial class EditGame
 {
     private Game game = new() { Name = string.Empty, Genre = string.Empty, ReleaseDate = DateTime.UtcNow };
+
+    private void HandleSubmit()
+    {
+        GameClient.AddGame(game);
+    }
+
 }
--- Games-Store/GamesStore.Web/Services/GameClient.cs
+++ Games-Store/GamesStore.Web/Services/GameClient.cs
@@ -7,11 +7,13 @@ public static class GameClient
     static private readonly List<Game> games =
     [
         new Game { Id = 1, Name = "Super Mario Bros", Genre = "Platformer", Price = 59.99M, ReleaseDate = new DateTime(1985, 09, 13) },
-        new Game() { Id = 2, Name = "Street Fighter II", Genre = "Fighting", Price = 19.99M, ReleaseDate = new DateTime(1991, 2, 1) },
-        new Game() { Id = 3, Name = "Final Fantasy XIV", Genre = "Roleplaying", Price = 59.99M, ReleaseDate = new DateTime(2010, 9, 30) },
-        new Game() { Id = 4, Name = "FIFA 23", Genre = "Sports", Price = 69.99M, ReleaseDate = new DateTime(2022, 9, 27) },
+        new Game { Id = 2, Name = "Street Fighter II", Genre = "Fighting", Price = 19.99M, ReleaseDate = new DateTime(1991, 2, 1) },
+        new Game { Id = 3, Name = "Final Fantasy XIV", Genre = "Roleplaying", Price = 59.99M, ReleaseDate = new DateTime(2010, 9, 30) },
+        new Game { Id = 4, Name = "FIFA 23", Genre = "Sports", Price = 69.99M, ReleaseDate = new DateTime(2022, 9, 27) },
         new Game { Id = 5, Name = "Super Mario Bros 2", Genre = "Platformer", Price = 59.99M, ReleaseDate = new DateTime(1988, 10, 09) },
     ];
 
     public static Game[] GetGames() => [.. games];
+
+    public static void AddGame(Game game) => games.Add(game);
 }
