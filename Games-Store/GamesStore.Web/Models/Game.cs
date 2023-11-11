83fdf9e5e77fcdf1d698f9250d31b99fbd3a8c82
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-11 10:59:52 +0530

    Validation Summary

--- Games-Store/GamesStore.Web/Models/Game.cs
+++ Games-Store/GamesStore.Web/Models/Game.cs
@@ -6,12 +6,15 @@ public class Game
 {
     public int Id { get; set; }
 
+    [Required]
     [StringLength(50)]
-    public required string Name { get; set; }
+    public string? Name { get; set; }
 
+    [Required]
     [StringLength(20)]
-    public required string Genre { get; set; }
+    public string? Genre { get; set; }
 
+    [Required]
     [Range(1, 100)]
     public decimal Price { get; set; }
 
