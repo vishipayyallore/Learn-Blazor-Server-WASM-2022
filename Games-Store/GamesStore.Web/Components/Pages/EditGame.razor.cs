76ea027c14face0bc63bb0a2745c2ad477f8097d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-14 00:44:01 +0530

    Swamy/11nov blazor full stackapp day3 (#28)
    * Update EditGame.razor
    
    * Update EditGame.razor
    
    * Validation Summary
    
    * Update GameClient.cs
    
    * Changes
    
    * Update EditGame.razor.cs
    
    * Changes
    
    * Update EditGame.razor

--- Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
+++ Games-Store/GamesStore.Web/Components/Pages/EditGame.razor.cs
@@ -9,11 +9,48 @@ public partial class EditGame
     [Inject]
     public NavigationManager NavigationManager { get; set; } = default!;
 
-    private Game game = new() { Name = string.Empty, Genre = string.Empty, ReleaseDate = DateTime.UtcNow };
+    [Parameter]
+    public int? Id { get; set; }
+
+    private Game? game;
+
+    private string title = string.Empty;
+
+    protected override void OnParametersSet()
+    {
+        if (Id is not null)
+        {
+            Game gameFound = GameClient.GetGame(Id.Value);
+
+            game = new()
+            {
+                Id = gameFound.Id,
+                Name = gameFound.Name,
+                Genre = gameFound.Genre,
+                Price = gameFound.Price,
+                ReleaseDate = gameFound.ReleaseDate
+            };
+
+            title = $"Edit {game.Name}";
+        }
+        else
+        {
+            game = new() { Name = string.Empty, Genre = string.Empty, ReleaseDate = DateTime.UtcNow };
+
+            title = "New Game";
+        }
+    }
 
     private void HandleSubmit()
     {
-        GameClient.AddGame(game);
+        if (game?.Id == 0)
+        {
+            GameClient.AddGame(game);
+        }
+        else
+        {
+            GameClient.UpdateGame(game);
+        }
 
         NavigationManager.NavigateTo("/");
     }
