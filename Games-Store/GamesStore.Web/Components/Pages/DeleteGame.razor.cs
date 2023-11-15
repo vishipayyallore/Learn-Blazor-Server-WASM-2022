f3eeaace498b0ca693c9a090ca42fcfaea52ce9e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-15 15:26:04 +0530

    Delete Dialog

---
+++ Games-Store/GamesStore.Web/Components/Pages/DeleteGame.razor.cs
@@ -0,0 +1,30 @@
+﻿using GamesStore.Web.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace GamesStore.Web.Components.Pages;
+
+public partial class DeleteGame
+{
+    [Parameter]
+    public Game? Game { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> OnClose { get; set; }
+
+    private string title = string.Empty;
+
+    protected override void OnParametersSet()
+    {
+        title = $"Delete {Game?.Name}?";
+    }
+
+    private void Confirm()
+    {
+        OnClose.InvokeAsync(true);
+    }
+
+    private void Cancel()
+    {
+        OnClose.InvokeAsync(false);
+    }
+}
