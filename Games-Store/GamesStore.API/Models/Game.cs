ff234f37f1eba4197b0b3299474724b386ad7cda
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-17 00:21:47 +0530

    Swamy/16nov blazor full stackapp day4 (#32)
    * Initial Version
    
    * Build.Proj
    
    * Moved the files into build folder
    
    * Changes
    
    * Update launch.json
    
    * Update tasks.json
    
    * Changes
    
    * Update launch.json
    
    * Update launch.json
    
    * Update launchSettings.json
    
    * Create Game.cs
    
    * Update Program.cs
    
    * Create games.http

---
+++ Games-Store/GamesStore.API/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesStore.API.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string? Name { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string? Genre { get; set; }
+
+    [Required]
+    [Range(1, 100)]
+    public decimal Price { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+}
